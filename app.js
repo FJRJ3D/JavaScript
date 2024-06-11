08f24d79cefa25cf413337c03e43962e500f52a3
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-11 17:33:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function VerificarClave(){
+
+    if (document.getElementById("ingreso").value=="FactoriaF5") {
+
+            document.getElementById("mensaje").innerHTML="Clave correcta";
+
+
+    } else {
+
+        document.getElementById("mensaje").innerHTML="Clave incorrecta";
+
+    }
+
+}
---
+++ javascript.js
@@ -0,0 +1,26 @@
+//VARIABLES
+
+let nombre = "Hello World";
+let numero = 3;
+let activado = true;
+let decimales = 3.5;
+
+
+//CONDICIONAL
+if (numero > 5) {
+    
+} else {
+    
+}
+
+//WHILE LOOP
+while (contador < 5) {
+    
+}
+
+//FOR LOOP
+
+for (let i = 0; i < 5; i++) {
+    
+}
+
